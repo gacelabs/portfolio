86dad7b15a7ac15d959ad4a329244be445bf3b4c
Eddie <eddie@annix.com.au>
2023-08-10 13:42:41 +0800

    updates

--- js/site.js
+++ js/site.js
@@ -8,4 +8,20 @@ $(document).ready(function () {
 	});
 
 	$('img, iframe').removeClass('hide');
+
+	$('.value-multiplier strong').each(function (i, elem) {
+		var limit = parseInt($(elem).attr('limit'));
+		setCounter(0, limit, elem);
+	});
 });
+
+let setCounter = function (count, limit, elem) {
+	var i = setInterval(() => {
+		count++;
+		elem.innerHTML = count + '%';
+		if (count >= limit) {
+			count = 0;
+			clearInterval(i);
+		}
+	}, 10);
+}
